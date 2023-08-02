d10f274f5cc3154db31cc98559601549e2781eec
thasipablo <thasipablo@gmail.com>
2023-08-02 08:28:18 -0400

    add save method and empty score list warning message

--- src/index.js
+++ src/index.js
@@ -1,30 +1,35 @@
-import "./style.css";
+import './style.css';
 
-import scores from "./scoreFunctions.js";
+import { scores, addScore } from './scoreFunctions.js';
 
 const renderTasks = () => {
-  const scoresElement = document.querySelector(".scores-list");
-  scores.innerHTML = ""; // Clear the previous content
+  const scoresElement = document.querySelector('.scores-list');
+  scoresElement.innerHTML = ''; // Clear the previous content
 
   // Create score items
-  scores.forEach((score) => {
-    const scoreElement = document.createElement("li");
-    scoreElement.classList.add("score-item");
-    scoreElement.innerHTML = `
-      ${score.name}: ${score.score}
-    `;
-    scoresElement.appendChild(scoreElement);
-  });
-  console.log(scoresElement);
-
+  if (scores.length > 0) {
+    scores.forEach((score) => {
+      const scoreElement = document.createElement('li');
+      scoreElement.classList.add('score-item');
+      scoreElement.innerHTML = `
+        ${score.name}: ${score.score}
+      `;
+      scoresElement.appendChild(scoreElement);
+    });
+  } else {
+    const warning = document.createElement('li');
+    warning.classList.add('warning');
+    warning.innerHTML = 'Your scores will appear here.';
+    scoresElement.appendChild(warning);
+  }
 };
 
 const handleAddTask = (e) => {
   e.preventDefault();
-  
+
   const name = document.querySelector('.name-input');
   const score = document.querySelector('.score-input');
-  scores.push({name: name.value.trim(), score: score.value.trim()})
+  addScore({ name: name.value.trim(), score: score.value.trim() });
 
   name.value = '';
   score.value = '';
@@ -32,8 +37,8 @@ const handleAddTask = (e) => {
   renderTasks();
 };
 
-const form = document.querySelector(".form");
-form.addEventListener("submit", handleAddTask);
+const form = document.querySelector('.form');
+form.addEventListener('submit', handleAddTask);
 
 // Render the tasks on page load
 renderTasks();
--- src/scoreFunctions.js
+++ src/scoreFunctions.js
@@ -1,3 +1,12 @@
-const scores = JSON.parse(localStorage.getItem("scores")) || [];
+const scores = JSON.parse(localStorage.getItem('scores')) || [];
 
-export default scores;
+const saveScore = () => {
+  localStorage.setItem('scores', JSON.stringify(scores));
+};
+
+const addScore = (score) => {
+  scores.push(score);
+  saveScore();
+};
+
+export { scores, addScore };
