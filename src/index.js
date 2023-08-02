bed3835e8e774446621dcbeccffde0aa46d6b4e5
thasipablo <thasipablo@gmail.com>
2023-08-02 07:03:38 -0400

    add the add neew score feature

--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
-import './style.css';
+import "./style.css";
+
+import scores from "./scoreFunctions.js";
 
 const renderTasks = () => {
-  const app = document.getElementById('app');
-  app.innerHTML = ''; // Clear the previous content
+  const scoresElement = document.querySelector(".scores-list");
+  scores.innerHTML = ""; // Clear the previous content
+
+  // Create score items
+  scores.forEach((score) => {
+    const scoreElement = document.createElement("li");
+    scoreElement.classList.add("score-item");
+    scoreElement.innerHTML = `
+      ${score.name}: ${score.score}
+    `;
+    scoresElement.appendChild(scoreElement);
+  });
+  console.log(scoresElement);
+
 };
 
 const handleAddTask = (e) => {
   e.preventDefault();
+  
+  const name = document.querySelector('.name-input');
+  const score = document.querySelector('.score-input');
+  scores.push({name: name.value.trim(), score: score.value.trim()})
+
+  name.value = '';
+  score.value = '';
+
+  renderTasks();
 };
 
-const form = document.querySelector('.form');
-form.addEventListener('submit', handleAddTask);
+const form = document.querySelector(".form");
+form.addEventListener("submit", handleAddTask);
 
 // Render the tasks on page load
 renderTasks();
---
+++ src/scoreFunctions.js
@@ -0,0 +1,3 @@
+const scores = JSON.parse(localStorage.getItem("scores")) || [];
+
+export default scores;
